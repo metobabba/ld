bb2b663d9b260a975d23f72944ed3f29e18a5f86
metobabba <91296039+metobabba@users.noreply.github.com>
2023-06-26 21:43:33 +0300

    Add files via upload

---
+++ load_clip_model.py
@@ -0,0 +1,2 @@
+from models.clipseg import CLIPDensePredT
+model = CLIPDensePredT(version='ViT-B/16', reduce_dim=64)
---
+++ load_sd_model.py
@@ -0,0 +1,2 @@
+from diffusers import StableDiffusionInpaintPipeline
+pipe = StableDiffusionInpaintPipeline.from_pretrained("lioo/Realistic_Vision_V2.0-inpainting")
